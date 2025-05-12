7520d540ce668c0af08fea5e2a60abfd283991a9
trinhthaominh <92960404+trinhthaominh@users.noreply.github.com>
2025-05-12 18:36:09 +0700

    Update Receptionist.cs

---
+++ QuanlyPhongKham/Models/Receptionist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanlyPhongKham.Models
+{
+    public class Receptionist : User
+    {
+        public Receptionist(string userName, string password, string email) : base(userName, password, email)
+        {
+        }
+    }
+}
